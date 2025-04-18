d22d751075fd788dc1ba890fe3e74087bff31754
Jackie Davis <jackiedavis5428@gmail.com>
2025-04-18 07:58:54 -0600

    separated JS file and added circle markers with corresponding colors

--- Main.js
+++ Main.js
@@ -1,57 +1,40 @@
-"use strict"; 
-function pointToCircle(feature, latlng)
+//below sets the basemap and initial view
+var map = L.map('map').setView([35.084196, -106.621010], 16);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//below adds a variable for the markers
+function pointToCircle(feature, latlng) {
 
   var fillColor_Var = "";
-  if feature.properties["Legal"] = "Illegal" {
-    fillColor_Var = "red";
-  } 
-  else {
+  //defines the "traffic control" variable as the one to set the color
+  if (feature.properties["Traffic Control"] === "Stop Sign") {
+    fillColor_Var = "Red";
+  } else if (feature.properties["Traffic Control"] === "Stoplight") {
     fillColor_Var = "green";
+  } else if (feature.properties["Traffic Control"] === "Paint") {
+    fillColor_Var = "#FFCC00";
+  } else {
+    fillColor_Var = "black";
   }
-
+  //sets the other properties of each circle marker
   var geojsonMarkerOptions = {
-    radius: 8,
-    //fillColor: "#F46B06",
+    radius: 7,
     fillColor: fillColor_Var,
     color: "black",
     weight: 1,
     opacity: 1,
-    fillOpacity: 0.8
+    fillOpacity: 1
   };
+  //defines the variable circlemarker as the layer using the latlong of the geoJSON and the options defined above
+  var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
 
-function addPopups(feature, layer) {
-//this if statemetn used to check whether the feature has a property named "Location"
-layer.bindPopup(feature.properties["Traffic Control"]);
+  return circleMarker;
 }
 
-
-/* initialize the map. 
-setView method: Sets the view of the map (geographical center and zoom) with the given animation options.
-
-setView( <LatLng> center, <Number> zoom?, <zoom/pan options> options? )
-*/
-
-var map = L.map('mapId').setView([39.9526,-75.1652], 12);
-
-//replace the code below from the Plain JavaScript from the map style you choose
-//at http://leaflet-extras.github.io/leaflet-providers/preview/
-
-var OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-});
-
-//change the varialbe "OpenTopoMap" according to the map style you choose
-//It is the varialbe between "var" and "= L.titleLayer(..."  in Line 28 in this script
-OpenStreetMap_HOT.addTo(map);
-
-//L.geoJSON(bikeThefts, {
-//  onEachFeature: addPopups
-//}).addTo(map);
-
-var bikesLayerGroup = L.geoJSON(bikeThefts, {
-onEachFeature: addPopups,
-pointToLayer: pointToCircle,
-});
-
-map.addLayer(bikesLayerGroup);
+//adds the circle markers to the map
+L.geoJSON(Crossings, {
+  pointToLayer: pointToCircle
+}).addTo(map);
